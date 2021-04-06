94130f20edd6806ac5d273c02593edfbdcb82531
Kiran Bhanushali <kiranbhanushali143.kb143@gmail.com>
2021-04-07 04:55:19 +0530

    init

---
+++ parser.y
@@ -0,0 +1,113 @@
+%{
+#include<stdio.h>
+int yyerror(char * m);
+int yylex();
+
+// will store 
+int num1  =0 ; 
+int num2 =0 ; 
+int operator_c = 0 ; 
+int ret_type = 0 ; 
+int evaluate();
+%}
+
+
+// customized yylval 
+%union 
+{
+    int num ;
+    int op;
+    int ret_type;
+};
+
+
+// token declaration
+%token<num> NUM 
+%token<op> LARGER BIGGER SMALLER LESSER 
+%token WHICH IS NUMBER OR FROM AND THAN BETWEEN EOL OTHER NL THE
+
+// rules 
+%%
+
+statement : WHICH IS the operator stm1 EOL { ret_type = 0;  operator_c = $<op>4; evaluate();  return 0 ;}
+
+          | FROM num AND num WHICH IS operator EOL  { ret_type = 0;num1=$<num>2 ; num2=$<num>4;  operator_c = $<op>7; evaluate();  return 0 ;}
+
+          | IS num operator THAN num EOL  { ret_type = 1; num1=$<num>2 ; num2=$<num>5;  operator_c = $<op>3; evaluate();  return 0 ;}
+
+
+stm1:    NUMBER num OR num { num1 = $<num>2 ; num2 = $<num>4 ; printf("number is: %d and %d\n ", $<num>2  , $<num>4); }
+
+     |   BETWEEN num AND num { num1 = $<num>2 ; num2 = $<num>4 ; printf("number is: %d and %d\n ", $<num>2  , $<num>4); }
+    ;
+
+operator: LESSER
+        | LARGER
+        | BIGGER
+        | SMALLER
+        ;
+
+num : NUM
+    ;
+
+the : THE 
+    |  /*epsilon*/ ; 
+
+%%
+
+int yyerror(char * message){
+
+    printf("Input Sentence is Invalid! : %s\n",message);
+    exit(1);
+
+}
+
+int min ( int a , int b ) {
+    if( a< b ) return a;
+    return b ;
+}
+
+int max ( int a , int b ) {
+    if( a> b ) return a;
+    return b ;
+}
+
+int evaluate(){
+    // ret_type == 0 means we have to return the number and ret_type == 1 meand we have to validate the comparision.
+    // operator_c == 0 is for smaller and lesser    
+    // operator_c == 1 is for larger and bigger
+
+    printf("Number1 is %d and Number2 is %d \n",num1,num2);
+    printf("--------------------------------------------------------\n");
+    if( ret_type ==0 ) {
+        if( operator_c == 0 ) {
+            printf("Ans: %d\n",min(num1,num2));
+        }else{
+            printf("Ans: %d\n",max(num1,num2));
+        }
+    }else{
+        // true false answer
+        if(operator_c==0){
+            if( num1 < num2 ) {
+                printf("True\n");
+            }else{
+                printf("False\n");
+            }
+        }else{
+            if( num1 > num2 ) {
+                printf("True\n");
+            }else{
+                printf("False\n");
+            }
+        }
+    }
+    printf("--------------------------------------------------------\n");
+}
+int main(){
+
+    yyparse();
+
+}
+
+
+
---
+++ y.tab.h
@@ -0,0 +1,122 @@
+/* A Bison parser, made by GNU Bison 3.7.3.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    NUM = 258,                     /* NUM  */
+    LARGER = 259,                  /* LARGER  */
+    BIGGER = 260,                  /* BIGGER  */
+    SMALLER = 261,                 /* SMALLER  */
+    LESSER = 262,                  /* LESSER  */
+    WHICH = 263,                   /* WHICH  */
+    IS = 264,                      /* IS  */
+    NUMBER = 265,                  /* NUMBER  */
+    OR = 266,                      /* OR  */
+    FROM = 267,                    /* FROM  */
+    AND = 268,                     /* AND  */
+    THAN = 269,                    /* THAN  */
+    BETWEEN = 270,                 /* BETWEEN  */
+    EOL = 271,                     /* EOL  */
+    OTHER = 272,                   /* OTHER  */
+    NL = 273,                      /* NL  */
+    THE = 274                      /* THE  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
+/* Token kinds.  */
+#define YYEOF 0
+#define YYerror 256
+#define YYUNDEF 257
+#define NUM 258
+#define LARGER 259
+#define BIGGER 260
+#define SMALLER 261
+#define LESSER 262
+#define WHICH 263
+#define IS 264
+#define NUMBER 265
+#define OR 266
+#define FROM 267
+#define AND 268
+#define THAN 269
+#define BETWEEN 270
+#define EOL 271
+#define OTHER 272
+#define NL 273
+#define THE 274
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+union YYSTYPE
+{
+#line 14 "parser.y"
+
+    int num ;
+    int op;
+    int ret_type;
+
+#line 110 "y.tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
